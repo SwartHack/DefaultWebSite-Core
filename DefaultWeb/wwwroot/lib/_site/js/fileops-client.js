afff77ce35875f47fb75e6cbc42d633db3f504dc
Eric Swartling <eks68@msn.com>
2017-10-11 00:28:29 -0600

    Add ignored files. Not sure why they were ignored?

---
+++ DefaultWeb/wwwroot/lib/_site/js/fileops-client.js
@@ -0,0 +1,172 @@
+﻿//////////////////////////////////////////////////////////////////////
+/// File ops client module
+//////////////////////////////////////////////////////////////////////
+define('dws/fileops-client', ['dws/controller', 'dws/model'],
+    function (Control, ViewModel) {
+
+        ///////////////////////////////////////////////////////////////////////
+        /// init cause it controls async html content not present at site load
+        /// we could pre-load everything, or lazy/late load like this
+        /// pros and cons....TODO
+        //////////////////////////////////////////////////////////////////////
+        function init() {
+            $("#file-input").change(function (evt) {
+                MultiplefileSelected(evt);
+            });
+            $("form#file-upload button[id=Cancel_btn]").click(function () {
+                Cancel_btn_handler()
+            });
+            $('a#file-upload-open').on('click', function () {
+
+                var options = {
+                    minWidth: 500,
+                    height: 'auto',
+                    modal: true,
+                    title: 'Upload Files'
+                };
+
+                $('#file-ops-client').dialog(options);
+            });
+            
+            var dropZone = document.getElementById('file-upload-drop');
+            dropZone.addEventListener('dragover', handleDragOver, false);
+            dropZone.addEventListener('drop', MultiplefileSelected, false);
+            dropZone.addEventListener('dragenter', dragenterHandler, false);
+            dropZone.addEventListener('dragleave', dragleaveHandler, false);
+            $.blockUI.defaults.overlayCSS = {
+                backgroundColor: '#000',
+                opacity: 0.6
+            };
+            //$.blockUI.defaults.css = {
+            //    padding: 0,
+            //    margin: 5,
+            //    width: '60%',
+            //    top: '30%',
+            //    left: '20%',
+            //    color: '#000',
+            //    border: '3px solid #aaa',
+            //    backgroundColor: '#fff'
+            //};
+            //$.blockUI({ message: $('#file-ops-client') });
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+        /// 
+        //////////////////////////////////////////////////////////////////////
+        function MultiplefileSelected(evt) {
+            evt.stopPropagation();
+            evt.preventDefault();
+            $('#drop_zone').removeClass('hover');
+
+            ViewModel.uploadFiles = evt.target.files || evt.dataTransfer.files;
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+        ///  file upload api async call
+        //////////////////////////////////////////////////////////////////////
+        $(document).on('submit', 'form#file-upload', function (e) {
+
+            e.preventDefault();
+            var $form = $(this);
+
+            //disable for once and for all TODO
+            $form.attr('disabled', true); // does not seem to work?!
+
+            var formData = new FormData($form[0]);
+            var settings = {
+                url: '/api/dws/upload',  //Server web api
+                type: 'POST',
+                xhr: function () {  // Custom XMLHttpRequest
+                    var myXhr = $.ajaxSettings.xhr();
+                    if (myXhr.upload) { // Check if upload property exists
+                        myXhr.upload.addEventListener('progress', progressHandlingFunction, false); // For handling the progress of the upload
+                    }
+                    return myXhr;
+                },
+                // Form data
+                data: formData,
+                //Options to tell jQuery not to process data or worry about content-type.
+                cache: false,
+                contentType: false,
+                processData: false
+            };
+
+            $.ajax(settings)
+                .done(function (data, textStatus, xhr) {
+                    if (data.statusCode == 200) {
+                        $('#serverFilesList tr:last').after(data.NewRow);
+                        alert(data.status);
+                    }
+                    else {
+                        alert(data.status);
+                    }
+                })
+                .fail(function (xhr, textStatus, error) {
+                    ViewModel.aborted(xhr, textStatus, error);
+                })
+                .always(function () {
+                    $('#client-container').empty();
+                    $('.create-file-link').show();
+                    $.unblockUI();
+                    ViewModel.waitEffects(false);
+                });
+        });
+
+        ///////////////////////////////////////////////////////////////////////
+        /// update upload progress
+        /// TODO progress per file
+        //////////////////////////////////////////////////////////////////////
+        function progressHandlingFunction(e) {
+            if (e.lengthComputable) {
+                var percentComplete = Math.round(e.loaded * 100 / e.total);
+                $("#fileProgress").css("width", percentComplete + '%').attr('aria-valuenow', percentComplete);
+                $('#fileProgress span').text(percentComplete + "%");
+            }
+            else {
+                $('#fileProgress span').text('unable to compute');
+            }
+        }
+        
+        ///////////////////////////////////////////////////////////////////////
+        /// Drop zone drag and drop stuff
+        //////////////////////////////////////////////////////////////////////
+        function handleDragOver(evt) {
+            evt.preventDefault();
+            evt.dataTransfer.effectAllowed = 'copy';
+            evt.dataTransfer.dropEffect = 'copy';
+        }
+
+        function dragenterHandler() {
+            //$('#drop_zone').removeClass('drop_zone');
+            $('#drop_zone').addClass('hover');
+        }
+
+        function dragleaveHandler() {
+            $('#drop_zone').removeClass('hover');
+        }
+
+        function OnDeleteAttachmentSuccess(data) {
+
+            if (data.ID && data.ID != "") {
+                $('' + data.ID).fadeOut('slow');
+            }
+            else {
+                alert("Unable to Delete");
+                console.log(data.message);
+            }
+        }
+
+        function Cancel_btn_handler() {
+            $('#clinet-container').empty();
+            $('.create-file-link').show();
+            $.unblockUI();
+            ViewModel.waitEffects(false);
+        }
+
+        return {
+            init: init,
+            progressHandlingFunction: progressHandlingFunction,
+            OnDeleteAttachmentSuccess: OnDeleteAttachmentSuccess,
+            Cancel_btn_handler: Cancel_btn_handler
+        }
+    });
---
+++ DefaultWeb/wwwroot/lib/_site/js/fileops-content.js
@@ -0,0 +1,180 @@
+﻿//////////////////////////////////////////////////////////////////////
+/// File operations content module
+//////////////////////////////////////////////////////////////////////
+define('dws/fileops-content', ['dws/controller', 'dws/model'],
+    function (Control, viewModel) {
+
+        function init() {
+
+            $('#content-left').on("click", function (e) {
+                contentNext();
+            });
+
+            $('#content-right').on("click", function (e) {
+                contentNext();
+            });
+
+            $('.main-content-area').on("swipeleft", function (e) {
+                contentNext();
+            });
+
+            $('.main-image').on("swiperight", function (e) {
+                contentNext();
+            });
+
+            $(document).on('keydown', '#main-content-area', function (e) {
+                if (!shortcutsEnabled) {
+                    return;
+                }
+
+                if (e.keyCode === 37) { //prev
+                    contentPrev();
+                    return false;
+                }
+
+                if (e.keyCode === 39) { //next
+                    contentNext();
+                    return false;
+                }
+            });
+
+            $(document).on('click', 'ul#thumbnails li a', function (e) {
+                e.preventDefault(); // what defaults?
+                var $link = $(this);
+                if (!$('img', $link).hasClass('selected')) { // if currently selected do nothing
+                    $(document).trigger('thumbnailclicked', $link);
+                    clickThumbnail($link); // will pushstate ???
+                }
+            });
+
+            hideAllContent();
+
+            getContent();
+
+            $('#col-util').hide();
+            $('#col-main').addClass('full-size');
+        }
+
+        var shortcutsEnabled = true;
+
+        function enableShortcuts() {
+            shortcutsEnabled = true;
+        }
+
+        function disableShortcuts() {
+            shortcutsEnabled = false;
+        }
+
+
+        function getContent() {
+
+            var settings = {
+                url: "/api/dws/list",
+                cache: false
+            }
+            viewModel.waitingTarget('#navbar-main');
+            viewModel.waiting(true);
+            //// integrate into dispatcher.js  TODO
+            $.ajax(settings)
+                .done(function (data, textStatus, xhr) {
+                    viewModel.fileInfo([]);
+                    viewModel.fileInfo(data.fileInfo);
+                })
+                .fail(function (xhr, textStatus, error) {
+                    viewModel.abort(xhr, textStatus, error);
+                })
+                .always(function (data, textStatus, xhr) {
+                    viewModel.waiting(false);
+                });
+            
+        }
+
+
+        function showContent(selector) {
+            if (!$(selector).is(':visible')) {
+                hideAllContent();
+                $(selector).show(); // beware that using an animated show (fadeIn, etc) may conflict with the visibility check
+            }
+        }
+
+        function hideAllContent() {
+            $('.content-area').hide();
+        }
+
+        function contentNext() {
+            var linkNext = $('ul#thumbnails li a img.selected').closest('li').next().find(">:first-child").trigger('click');
+            $(linkNext).trigger('click');
+        }
+
+        function contentPrev() {
+            var linkPrev = $('ul#thumbnails li a img.selected').closest('li').prev().find(">:first-child");
+            $(linkPrev).trigger('click');
+        }
+
+        function clickThumbnail($link) {
+            var fileURL = $link.attr('href');
+            var virtualPath = $link.attr('data-virtual-path');
+            var mimeType = $link.attr('data-mime-type');  //added to template!!!
+
+            //learn what this is doing
+            window.history && window.history.pushState && window.history.replaceState({ image: "", virtualPath: "" }, "", "");
+            openFile(virtualPath, mimeType);
+        }
+
+        function openFile(virtualPath, mimeType) {
+            var $thumbnail = $('a[data-virtual-path = "' + virtualPath + '"] img');
+            styleSelectedThumbnail($thumbnail);
+            loadContent(virtualPath, mimeType);
+        }
+
+        function styleSelectedThumbnail($thumbnail) {
+            $('ul#thumbnails li a img').removeClass("selected");
+            $thumbnail.addClass("selected");
+        }
+
+        function empty() {
+            viewModel.thumbnails([]);
+        }
+
+
+        function loadContent() {
+
+            var settings = {
+                url: '/api/dws/view/{id}',  //Server web api
+                type: 'Get',
+                cache: false
+            };
+
+            $.ajax(settings)
+                .done(function (data, textStatus, xhr) {
+                    if (data.statusCode == 200) {
+                        
+                    }
+                    else {
+                        alert(data.status);
+                    }
+                })
+                .fail(function (xhr, textStatus, error) {
+                    viewModel.aborted(xhr, textStatus, error);
+                })
+                .always(function () {
+                    $('#clinet-container').empty();
+                    $('.create-file-link').show();
+                    $.unblockUI();
+                    viewModel.waitEffects(false);
+                });
+
+
+        }
+
+        return {
+            init:init,
+            showContent: showContent,
+            hideAllContent: hideAllContent,
+            contentNext: contentNext,
+            contentPrev: contentPrev,
+            clickThumbnail: clickThumbnail
+           
+        };
+
+    });

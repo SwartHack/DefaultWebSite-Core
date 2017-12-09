2c8b3b28aee077653bcf9cc35eedf4f5c5b1c09c
Eric Swartling <eks68@msn.com>
2017-12-09 16:27:45 -0700

    Accounting for a few straglers. Trying VS TE git again...

---
+++ DefaultWeb/wwwroot/lib/_site/js/model-utils.js
@@ -0,0 +1,82 @@
+﻿//////////////////////////////////////////////////////////////////////
+/// KO viewModel extensions module
+//////////////////////////////////////////////////////////////////////
+define('dws/model-utils', function () {
+
+    //var dataURLFileReader = {
+    //    read: function (file, callback) {
+    //        var reader = new FileReader();
+    //        var fileInfo = {
+    //            name: file.name,
+    //            type: file.type,
+    //            fileContent: null,
+    //            size: function () {
+    //                var FileSize = 0;
+    //                if (file.size > 1048576) {
+    //                    FileSize = Math.round(file.size * 100 / 1048576) / 100 + " MB";
+    //                }
+    //                else if (file.size > 1024) {
+    //                    FileSize = Math.round(file.size * 100 / 1024) / 100 + " KB";
+    //                }
+    //                else {
+    //                    FileSize = file.size + " bytes";
+    //                }
+    //                return FileSize;
+    //            }
+    //        };
+    //        if (!file.type.match('image.*')) {
+    //            callback("file type not allowed", fileInfo);
+    //            return;
+    //        }
+    //        reader.onload = function () {
+    //            fileInfo.fileContent = reader.result;
+    //            callback(null, fileInfo);
+    //        };
+    //        reader.onerror = function () {
+    //            callback(reader.error, fileInfo);
+    //        };
+    //        reader.readAsDataURL(file);
+    //    }
+    //};
+
+    function callAborted(xhr, textStatus, error) {
+
+     }
+
+    ///////////////////////////////////////
+    /// this is for async ajax calls to server
+    //////////////////////////////////////
+    function waitStatus(status, target) {
+
+        if (status) {
+            $(target).addClass('waiting');
+        }
+        else {
+            $(target).removeClass('waiting');
+        }
+    }
+
+    ///////////////////////////////////////
+    /// this is for actual DOM element loading
+    ///////////////////////////////////////
+    function loadingStatus(status, target) {
+        var completed = $(target)[0].complete;
+        if (!completed) {
+            $(target)[0].addClass('loading');
+            $(target).load(function () {
+                $(target).removeClass('loading');
+            });
+        }
+    }
+
+
+
+
+    return {
+        waitStatus: waitStatus,
+        loadingStatus: loadingStatus,
+        callAborted: callAborted
+        
+    }
+
+});
---
+++ DefaultWeb/wwwroot/lib/_site/js/thumbnail.js
@@ -0,0 +1,106 @@
+﻿//////////////////////////////////////////////////////////////////////
+/// thumb client module
+//////////////////////////////////////////////////////////////////////
+define('dws/thumbnail', [],
+    function () {
+
+        //////////////////////////////////////////////////////////////////////
+        /// 
+        //////////////////////////////////////////////////////////////////////
+        function getThumbFromFile(file, callback) {
+
+            if ( file.type.match('image/*') ) {
+               thumbFromImageFile(file, callback);
+            }
+                    
+            if (file.type.match('application/*')) {
+                thumbFromAppFile(file, callback);
+            }
+
+            // unsupported/invalid
+
+        }
+
+        //////////////////////////////////////////////////////////////////////
+        /// 
+        //////////////////////////////////////////////////////////////////////
+        function thumbFromImageFile(file, callback) {
+            var reader = new FileReader();
+            reader.file = file;
+
+            reader.onload = (function (e) {
+                callback(reader.file,e.target.result);
+            });
+
+            reader.readAsDataURL(file);
+        }
+
+        //////////////////////////////////////////////////////////////////////
+        /// TODO
+        /// Gonna have to deal with each application type individually as we 
+        /// support more. Need a smarter way to do it as there are so many!!!
+        //////////////////////////////////////////////////////////////////////
+        function thumbFromAppFile(file, callback) {
+
+            if (file.type.match('*/pdf')) {
+                thumbFromPdf(file, callback);
+            }
+           
+        }
+
+         //////////////////////////////////////////////////////////////////////
+        /// 
+        //////////////////////////////////////////////////////////////////////
+        function thumbFromPdf(file, callback) {
+
+            PDFJS.workerSrc = 'pdf.worker.js';
+
+            var fullname = file.name;
+            PDFJS.getDocument(fullname).then(function (pdf) {
+
+                pdf.getPage(1).then(function (page) {
+                    var viewport = page.getViewport(0.5);
+                    var canvas = document.createElement('canvas');
+                    var ctx = canvas.getContext('2d');
+                    canvas.height = viewport.height;
+                    canvas.width = viewport.width;
+
+                    var renderContext = {
+                        canvasContext: ctx,
+                        viewport: viewport
+                    };
+
+                    page.render(renderContext).then(function () {
+                        //set to draw behind current content
+                        ctx.globalCompositeOperation = "destination-over";
+                        //set background color
+                        ctx.fillStyle = "#fff";
+                        //draw on entire canvas
+                        ctx.fillRect(0, 0, canvas.width, canvas.height);
+                        // create an img from the canvas which contains the page contents
+                        var img_src = canvas.toDataURL();
+
+                        callback(file, img_src);
+                    });
+
+                });
+
+
+            });
+
+        }
+
+         //////////////////////////////////////////////////////////////////////
+        /// 
+        //////////////////////////////////////////////////////////////////////
+        function thumbFromVideoFile() {
+
+        }
+
+         
+
+        return {
+            getThumbFromFile: getThumbFromFile
+        }
+
+    });
